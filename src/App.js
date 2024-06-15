52ab7477710018fee201b4ade3efe10e41264d82
yashgulia <yashgulia09@gmail.com>
2024-06-15 17:38:45 +0530

    Updating ReadME

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Namaste Everyone, Let's build Netflix GPT.</div>;
+  return <div className="App">Let's build Netflix GPT.</div>;
 }
 
 export default App;
