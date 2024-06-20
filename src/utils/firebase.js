a80cf6a0cfd9114842ac9a0a7e6dafe7d26cdffd
yashgulia <yashgulia09@gmail.com>
2024-06-21 00:23:16 +0530

    Firebase Deployment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYr13P-w1OZBeMpZ5Hk41z-d6yjdnxHRk",
-  authDomain: "netflixgpt-4f469.firebaseapp.com",
-  projectId: "netflixgpt-4f469",
-  storageBucket: "netflixgpt-4f469.appspot.com",
-  messagingSenderId: "205975300643",
-  appId: "1:205975300643:web:e8acbd94b45f98067d1f96",
-  measurementId: "G-54F4CR461W",
+  apiKey: "AIzaSyDwN8PLztjyp2gC1j-3Rb2aBiqnx7kDqvw",
+  authDomain: "netflixgpt-yashgulia.firebaseapp.com",
+  projectId: "netflixgpt-yashgulia",
+  storageBucket: "netflixgpt-yashgulia.appspot.com",
+  messagingSenderId: "665398525545",
+  appId: "1:665398525545:web:c1a94dcfd3c14bf8d6ecf8",
+  measurementId: "G-C11ZC9Y2FH",
 };
 
 // Initialize Firebase
